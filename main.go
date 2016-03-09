0bb73b84c6584392a2af8c266382706b0fbbb0e6
ekos <ekos.overdrive1@gmail.com>
2016-03-10 00:03:31 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "net/http"
+    "github.com/go-martini/martini"
+    "appengine"
+)
+
+func main() {
+  m := martini.Classic()
+  m.Use(AppEngine)
+  m.Get("/", func() string {
+    return "Hello world!"
+  })
+  http.Handle("/", m)
+}
+func AppEngine(c martini.Context, r *http.Request) {
+    c.MapTo(appengine.NewContext(r), (*appengine.Context)(nil))
+}
